b8ff6b8f4317244eed0321f925507d8ec4add006
Victor Kozlov <punkrus18@gmail.com>
2017-11-14 23:41:41 +0300

    Initial commit;

---
+++ config/webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    entry: "./src/main.js",
+    output: {
+        path: __dirname,
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            {
+                loader: 'babel-loader'
+            }
+        ]
+    }
+};
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import WebGl from './WebGl.js'
+let webGl
+window.onload = () => {
+  webGl = new WebGl()
+}
