1d02a555004dddb51a2297c9ca4da8a378a286c9
Victor Kozlov <punkrus18@gmail.com>
2017-11-16 18:22:56 +0300

    delete empty function;

--- src/Polygon.js
+++ src/Polygon.js
@@ -5,8 +5,6 @@ export default class Polygon {
     this.webGl.bindBuffer(webGl.ARRAY_BUFFER, this.positionBuffer)
     this.webGl.bufferData(this.webGl.ARRAY_BUFFER, new Float32Array(vertices), this.webGl.STATIC_DRAW)
   }
-  bufferData () {
-  }
   getPositionBuffer () {
     return this.positionBuffer
   }
