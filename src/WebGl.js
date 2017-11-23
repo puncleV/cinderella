84ec321d1cfc9b6133485cb52a5bbd9e29654fca
Victor Kozlov <punkrus18@gmail.com>
2017-11-23 13:32:41 +0300

    walking;

--- src/WebGl.js
+++ src/WebGl.js
@@ -9,14 +9,34 @@ export default class WebGl {
     this.mvMatrix = mat4.create()
     this.pMatrix = mat4.create()
     this.mvMatrixStack = []
+    this.worldVertices = []
+
+    this.walls = {
+      x: [],
+      z: []
+    }
+
+    this.currentlyPressedKeys = {}
+    this.pitch = 0
+    this.pitchRate = 0
+    this.yaw = 90
+    this.yawRate = 0
+    this.xPos = 4.7
+    this.yPos = 0.4
+    this.zPos = 2.43
+    this.speed = 0
+    this.lastTime = 0
+    this.joggingAngle = 0
 
     try {
       this.initGl()
       this.initShaders()
       this.initTexture(() => {
         this.loadWorld((worldText) => {
+          this.worldVertices = this.handleLoadedWorld(worldText)
           this.webGl.enable(this.webGl.DEPTH_TEST)
-          this.drawScene(this.handleLoadedWorld(worldText))
+          this.drawScene()
+          this.tick()
           console.info('SUCCESS: webGl initialized!')
         })
       })
@@ -31,55 +51,34 @@ export default class WebGl {
     this.webGl.clear(this.webGl.COLOR_BUFFER_BIT)
     this.webGl.viewportWidth = this.canvas.width
     this.webGl.viewportHeigth = this.canvas.height
+    document.onkeydown = this.handleKeyDown.bind(this)
+    document.onkeyup = this.handleKeyUp.bind(this)
   }
+
   degToRad (degrees) {
     return degrees * Math.PI / 180
   }
-  drawScene (worldVertices) {
+
+  drawScene () {
     this.webGl.viewport(0, 0, this.webGl.viewportWidth, this.webGl.viewportHeigth)
     this.webGl.clear(this.webGl.COLOR_BUFFER_BIT | this.webGl.DEPTH_BUFFER_BIT)
 
-    mat4.perspective(45, this.webGl.viewportWidth / this.webGl.viewportHeigth, 0.1, 100.0, this.pMatrix)
+    mat4.perspective(90, this.webGl.viewportWidth / this.webGl.viewportHeigth, 0.1, 100.0, this.pMatrix)
     mat4.identity(this.mvMatrix)
 
-    var pitch = 0
-    var pitchRate = 0
-
-    var yaw = 0
-    var yawRate = 0
-
-    var xPos = -0.5
-    var yPos = 0.4
-    var zPos = 2
-
-    var speed = 0
-
-    mat4.rotate(this.mvMatrix, this.degToRad(-pitch), [1, 0, 0])
-    mat4.rotate(this.mvMatrix, this.degToRad(-yaw), [0, 1, 0])
-    mat4.translate(this.mvMatrix, [-xPos, -yPos, -zPos])
-    // mat4.translate(this.mvMatrix, [0.5, -0.5, -1.6])
+    mat4.rotate(this.mvMatrix, this.degToRad(-this.pitch), [1, 0, 0])
+    mat4.rotate(this.mvMatrix, this.degToRad(-this.yaw), [0, 1, 0])
+    mat4.translate(this.mvMatrix, [-this.xPos, -this.yPos, -this.zPos])
 
     this.webGl.activeTexture(this.webGl.TEXTURE0)
     this.webGl.bindTexture(this.webGl.TEXTURE_2D, this.mainTexture)
     this.webGl.uniform1i(this.shaderProgram.samplerUniform, 0)
-    worldVertices.forEach(triangleVertices => {
+    this.worldVertices.forEach(triangleVertices => {
       let triangle = new Triangle(this.webGl, triangleVertices.vertices, triangleVertices.textureVertices)
       this.webGl.bindBuffer(this.webGl.ARRAY_BUFFER, triangle.getTextureCoordsBuffer())
       this.webGl.vertexAttribPointer(this.shaderProgram.textureCoordAttribute, triangle.getTextureCoordsBuffer().itemSize, this.webGl.FLOAT, false, 0, 0)
       this.bindAndDrawArray('TRIANGLES', triangle.getPositionBuffer())
     })
-    //
-    // gl.bindBuffer(gl.ARRAY_BUFFER, worldVertexPositionBuffer);
-    // gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, worldVertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
-    //
-    // setMatrixUniforms();
-    // gl.drawArrays(gl.TRIANGLES, 0, worldVertexPositionBuffer.numItems);
-
-    // this.webGl.drawArrays(this.webGl.TRIANGLES, 0, triangle.getPositionBuffer().numItems)
-
-    // mat4.translate(this.mvMatrix, [3.0, 0.0, 0.0])
-    // let square = new Square(this.webGl)
-    // this.bindAndDrawArray('TRIANGLE_STRIP', square.getPositionBuffer())
   }
 
   bindAndDrawArray (arrayType, vertexPositionBuffer) {
@@ -89,16 +88,50 @@ export default class WebGl {
     this.webGl.drawArrays(this.webGl[arrayType], 0, vertexPositionBuffer.numItems)
   }
 
+  animate () {
+    const timeNow = new Date().getTime()
+    if (this.lastTime !== 0) {
+      const elapsed = timeNow - this.lastTime
+
+      if (this.speed !== 0) {
+        const dX = Math.sin(this.degToRad(this.yaw)) * this.speed * elapsed
+        const dZ = Math.cos(this.degToRad(this.yaw)) * this.speed * elapsed
+        const newX = this.xPos - dX
+        const newZ = this.zPos - dZ
+        let xCorrect = !this.walls.x.some((xCoord, i) => {
+          const zCoord = this.walls.z[i]
+          const xPassed = (Math.abs(parseFloat(xCoord) - newX) < 0.1)
+          const zPassed = (Math.abs(parseFloat(zCoord) - newZ) < 0.1)
+          return xPassed && zPassed
+        })
+        // let zCorrect = !this.walls.z.some(zCoord => )
+        // console.log(this.xPosd)
+        if (xCorrect) {
+          this.xPos = newX
+          this.zPos = newZ
+        }
+
+        // if (zCorrect) {
+        // }
+
+        this.joggingAngle += elapsed * 0.6
+        this.yPos = Math.sin(this.degToRad(this.joggingAngle)) / 20 + 0.4
+      }
+
+      this.yaw += this.yawRate * elapsed
+      this.pitch += this.pitchRate * elapsed
+    }
+    this.lastTime = timeNow
+  }
+
   handleLoadedWorld (worldText) {
     let normalizedTriangles = []
     let verticesBuf = []
     let textureVerticesBuf = []
-    console.log(worldText.split('\n').filter(line => line.indexOf('//') === -1).map(line => line.split(/\s+/)))
     worldText.split('\n').filter(line => line.indexOf('//') === -1)
-      .map(line => line.split(/\s+/).filter(value => value !== ""))
+      .map(line => line.split(/\s+/).filter(value => value !== ''))
       .filter(line => line.length === 5)
       .forEach((line, i) => {
-        console.log(line)
         if (i !== 0 && i % 3 === 0) {
           normalizedTriangles.push({
             vertices: verticesBuf,
@@ -107,9 +140,18 @@ export default class WebGl {
           verticesBuf = []
           textureVerticesBuf = []
         }
-        verticesBuf = verticesBuf.concat(line.splice(0, 3))
+
+        const vertices = line.splice(0, 3)
+        verticesBuf = verticesBuf.concat(vertices)
         textureVerticesBuf = textureVerticesBuf.concat(line)
+        // if (this.walls.x.indexOf(vertices[0]) === -1) {
+          this.walls.x.push(vertices[0])
+        // }
+        // if (this.walls.z.indexOf(vertices[2]) === -1) {
+          this.walls.z.push(vertices[2])
+        // }
       })
+    console.log(this.walls)
     return normalizedTriangles
   }
 
@@ -210,9 +252,48 @@ export default class WebGl {
     this.shaderProgram.samplerUniform = this.webGl.getUniformLocation(this.shaderProgram, 'uSampler')
   }
   tick () {
-    this.requestAnimFrame(tick)
+    requestAnimFrame(this.tick.bind(this))
     this.handleKeys()
     this.drawScene()
     this.animate()
+  }
+  handleKeyDown (event) {
+    this.currentlyPressedKeys[event.keyCode] = true
+  }
+
+  handleKeyUp (event) {
+    this.currentlyPressedKeys[event.keyCode] = false
+  }
+
+  handleKeys () {
+    if (this.currentlyPressedKeys[33]) {
+      // Page Up
+      this.pitchRate = 0.1
+    } else if (this.currentlyPressedKeys[34]) {
+      // Page Down
+      this.pitchRate = -0.1
+    } else {
+      this.pitchRate = 0
+    }
+
+    if (this.currentlyPressedKeys[37] || this.currentlyPressedKeys[65]) {
+      // Left cursor key or A
+      this.yawRate = 0.1
+    } else if (this.currentlyPressedKeys[39] || this.currentlyPressedKeys[68]) {
+      // Right cursor key or D
+      this.yawRate = -0.1
+    } else {
+      this.yawRate = 0
+    }
+
+    if (this.currentlyPressedKeys[38] || this.currentlyPressedKeys[87]) {
+      // Up cursor key or W
+      this.speed = 0.001
+    } else if (this.currentlyPressedKeys[40] || this.currentlyPressedKeys[83]) {
+      // Down cursor key
+      this.speed = -0.001
+    } else {
+      this.speed = 0
+    }
   }
 }
